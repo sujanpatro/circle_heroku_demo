9fecdef28dad88a0b2fd08d74277fd941267e7fe
sujanpatro <sujanpatro@gmail.com>
2022-01-11 11:32:23 +0530

    testing CI/CD with first test change

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running on Heroku"
+    return "Flask app is running on Heroku\nI'm trying to change something to check the CI CD pipeline"
 
 
 port = int(os.getenv("PORT", 5001))
